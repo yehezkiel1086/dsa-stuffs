028f3052fb48101f4429b131df47f18b491633c4
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-04 14:24:28 +0700

    refactor(python): shorten 01-basic-data-types/02-runner-up

--- python/01-basic-data-types/02-runner-up.py
+++ python/01-basic-data-types/02-runner-up.py
@@ -2,7 +2,5 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     
-    l_arr = list(arr)
-    l_arr.sort()
-    s_arr = list(set(l_arr))
-    print(s_arr[-2])
+    l_arr = sorted(list(set(arr)))
+    print(l_arr[-2])
