29d1ffcbbc9723e53902b7ece86195d32d4a779c
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-05 11:56:28 +0700

    feat(algo): add sorting/00-warmup

--- notes/sorting.py
+++ notes/sorting.py
@@ -1,4 +1,17 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        m = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[m]:
+                m = j
+        arr[m], arr[i] = arr[i], arr[m]
+    return arr
+
 def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 def insertion_sort(arr):
@@ -11,6 +24,7 @@ def insertion_sort(arr):
 if __name__ == "__main__":
     arr = [7, 12, 9, 11, 3]
     # s_arr = insertion_sort(arr)
-    s_arr = bubble_sort(arr)
+    # s_arr = bubble_sort(arr)
+    s_arr = selection_sort(arr)
     
     print(s_arr)
