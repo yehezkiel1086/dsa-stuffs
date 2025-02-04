5afdb29e31c7f0104d27bc4f89afdfedba907226
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2025-02-04 20:43:38 +0700

    feat(cpp): add math

---
+++ cpp/math.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+  double x = 3, y = 4, z;
+
+  z = std::max(x, y);
+  z = std::min(x, y);
+  z = pow(2, 4);
+  z = sqrt(16);
+  z = abs(-3);
+  z = ceil(3.14);
+  z = floor(3.14);
+  z = round(3.14);
+
+  std::cout << z << std::endl;
+
+  return 0;
+}
