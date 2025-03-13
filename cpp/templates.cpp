0add3f5db522fb5f089b5cccd647ce67f9c05822
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-03-13 14:09:16 +0700

    feat(cpp): update templates.cpp

--- cpp/templates.cpp
+++ cpp/templates.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-template <typename T>
+template <typename T, typename U>
 
-T max(T a, T b) {
+T max(T a, U b) {
   return a > b ? a: b;
 }
 
 int main() {
   std::cout << max('a', 'z') << "\n";
-  std::cout << max(1000, -1) << "\n";
+  std::cout << max(1000, -1.2) << "\n";
   return 0;
 }
