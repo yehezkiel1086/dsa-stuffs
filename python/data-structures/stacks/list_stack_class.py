103dff7323af87981842ce462a9c17f3bc898b34
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-08-03 09:58:42 +0700

    feat(python): update data-structures/stacks/list_stack_class.py

--- python/data-structures/stacks/list_stack_class.py
+++ python/data-structures/stacks/list_stack_class.py
@@ -7,57 +7,37 @@ Stack Operations:
   Peek: Returns the top (last) element on the stack.
   isEmpty: Checks if the stack is empty.
   Size: Finds the number of elements in the stack.
-
-Reasons to implement stacks using lists/arrays:
-
-  Memory Efficient: Array elements do not hold the next elements address like linked list nodes do.
-  Easier to implement and understand: Using arrays to implement stacks require less code than using linked lists, and for this reason it is typically easier to understand as well.
-  A reason for not using arrays to implement stacks:
-
-  Fixed size: An array occupies a fixed part of the memory. This means that it could take up more memory than needed, or if the array fills up, it cannot hold more elements.
 """
 
 class Stack:
-  def __init__(self):
-    self.stack = []
-  
-  def push(self, element):
-    self.stack.append(element)
-  
+  def __init__(self, stack):
+    self.stack = stack
+
+  def push(self, item):
+    self.stack.append(item)
+
   def pop(self):
     return self.stack.pop()
-
+  
   def peek(self):
     return self.stack[-1]
-
-  def isEmpty(self):
+  
+  def is_empty(self):
     return not bool(self.stack)
   
-  def getSize(self):
+  def size(self):
     return len(self.stack)
-  
+
   def __str__(self):
-    return f"{self.stack}"
+    return self.stack
 
-myStack = Stack()
+myStack = Stack(['A', 'B'])
 
-# push
-myStack.push("a")
-myStack.push("b")
-myStack.push("c")
-myStack.push("d")
-myStack.push("e")
+myStack.push('C')
 
-print("Pushed:", myStack)
-
-# pop
-print("Popped:", myStack.pop(), myStack)
-
-# peek
-print("Peek:", myStack.peek())
-
-# isEmpty
-print("isEmpty:", myStack.isEmpty())
-
-# getSize
-print("Size:", myStack.getSize())
+print("Stack: ", myStack.stack)
+print("Pop: ", myStack.pop())
+print("Stack after Pop: ", myStack.stack)
+print("Peek: ", myStack.peek())
+print("isEmpty: ", myStack.is_empty())
+print("Size: ", myStack.size())
